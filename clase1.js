e5bffb6ddcbe4500a37fa8a81e9c220199fdfd80
Kevin Garcia <kevinede40@gmail.com>
2021-06-08 13:30:54 -0600

    Prueba GitHub Fundamentos Clase 1

---
+++ clase1.js
@@ -0,0 +1,6 @@
+/* console.log("Hola mundo JS!") */
+var nombre = 'Cooper';
+var apellido = 'Palacios';
+var edad = 21;
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad + ' años')
