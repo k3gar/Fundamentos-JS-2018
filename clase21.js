414926aab8460da2927871c5fb020acc392844f7
Kevin Garcia <kevinede40@gmail.com>
2021-06-11 15:33:59 -0600

    Mas clases

--- clase21.js
+++ clase21.js
@@ -1,5 +1,5 @@
 //Cómo funcionan las clases en JavaScript
-
+/* 
 function Persona(nombre, apellido, altura){
     this.nombre = nombre
     this.apellido = apellido
@@ -9,7 +9,31 @@ Persona.prototype.saludar = function(){
     console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y mi altura es ${this.altura}`)
 }
 
-var kevin = new Persona('Kevin', 'Garcia', 170)
+const altura = 180
+
+var kevin = new Persona('Kevin', 'Garcia', 180)
 var cooper = new Persona('cooper', 'palacios', 50)
 kevin.saludar()
+cooper.saludar() */
+
+function Persona(nombre, apellido, altura){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+Persona.prototype.saludar = function(){
+    if(this.altura == 180){
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y mi altura es ${this.altura}cm. SOY ALTO!`)
+    }
+    else{
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y mi altura es ${this.altura}cm. No soy alto!`)
+    }
+    debugger
+}
+
+const altura = 180
+
+//var kevin = new Persona('Kevin', 'Garcia', 180)
+var cooper = new Persona('cooper', 'palacios', 50)
+//kevin.saludar()
 cooper.saludar()
